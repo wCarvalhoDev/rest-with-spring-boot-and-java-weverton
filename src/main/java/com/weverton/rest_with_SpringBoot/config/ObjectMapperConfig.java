ac42f42ab3a348aa37de26a3eedb778024d24ced
Weverton Carvalho <wcarvalhomarcelino@gmail.com>
2025-12-14 09:20:59 -0300

    Custom JSON Serialization

---
+++ src/main/java/com/weverton/rest_with_SpringBoot/config/ObjectMapperConfig.java
@@ -0,0 +1,22 @@
+package com.weverton.rest_with_SpringBoot.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObjectMapperConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+
+        SimpleFilterProvider filters = new SimpleFilterProvider()
+                .addFilter("PersonFilter",
+                        SimpleBeanPropertyFilter.serializeAllExcept("sensitiveData"));
+        mapper.setFilterProvider(filters);
+        return mapper;
+    }
+}
