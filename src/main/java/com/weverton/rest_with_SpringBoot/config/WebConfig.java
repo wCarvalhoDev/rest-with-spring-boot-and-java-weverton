2c7f98310bf983a5ca0c91e82d629f05a972a889
Weverton Carvalho <wcarvalhomarcelino@gmail.com>
2026-01-08 10:12:17 -0300

    solução do problema de CORS, configuração global

--- src/main/java/com/weverton/rest_with_SpringBoot/config/WebConfig.java
+++ src/main/java/com/weverton/rest_with_SpringBoot/config/WebConfig.java
@@ -1,28 +1,30 @@
 package com.weverton.rest_with_SpringBoot.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${cors.originPatterns:default}")
+    private String CorsOriginPatterns = "";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        var allowedOrigins = CorsOriginPatterns.split(",");
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                //.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedMethods("*")
+                .allowCredentials(true);
+    }
+
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-
-        //via EXTENSION. http://localhost:8080/api/person/v1/2.xml or http://localhost:8080/api/person/v1/2.JSON Deprecated on Spring Boot 2.6
-
-        //via QUERY PARAM  http://localhost:8080/api/person/v1/2?mediaType=xml
-
-        /** configurer.favorParameter(true)
-                .parameterName("mediaType")
-                .ignoreAcceptHeader(true)
-                .useRegisteredExtensionsOnly(false)
-                .defaultContentType(MediaType.APPLICATION_JSON)
-                .mediaType("json", MediaType.APPLICATION_JSON)
-                .mediaType("xml", MediaType.APPLICATION_XML);
-        */
 
         //via HEADER PARAM  http://localhost:8080/api/person/v1/
         configurer.favorParameter(false)
@@ -33,4 +35,5 @@ public class WebConfig implements WebMvcConfigurer {
                 .mediaType("xml", MediaType.APPLICATION_XML)
                 .mediaType("yaml", MediaType.APPLICATION_YAML);
     }
+
 }
--- src/main/java/com/weverton/rest_with_SpringBoot/controllers/PersonController.java
+++ src/main/java/com/weverton/rest_with_SpringBoot/controllers/PersonController.java
@@ -11,34 +11,37 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+// @CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/api/person/v1")
-@Tag(name = "People", description = "Endpoints for managing people")
+@Tag(name = "People", description = "Endpoints for Managing People")
 public class PersonController implements PersonControllerDocs {
 
     @Autowired
     private PersonServices service;
 
-    @GetMapping(
-            produces = {
-                    MediaType.APPLICATION_JSON_VALUE,
-                    MediaType.APPLICATION_XML_VALUE,
-                    MediaType.APPLICATION_YAML_VALUE})
+    @GetMapping(produces = {
+            MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_YAML_VALUE})
     @Override
     public List<PersonDTO> findAll() {
         return service.findAll();
     }
 
+   // @CrossOrigin(origins = "http://localhost:8080")
     @GetMapping(value = "/{id}",
             produces = {
                     MediaType.APPLICATION_JSON_VALUE,
                     MediaType.APPLICATION_XML_VALUE,
-                    MediaType.APPLICATION_YAML_VALUE})
+                    MediaType.APPLICATION_YAML_VALUE}
+    )
     @Override
     public PersonDTO findById(@PathVariable("id") Long id) {
         return service.findById(id);
     }
 
+   // @CrossOrigin(origins = {"http://localhost:8080","https://www.erudio.com.br"})
     @PostMapping(
             consumes = {
                     MediaType.APPLICATION_JSON_VALUE,
@@ -47,7 +50,8 @@ public class PersonController implements PersonControllerDocs {
             produces = {
                     MediaType.APPLICATION_JSON_VALUE,
                     MediaType.APPLICATION_XML_VALUE,
-                    MediaType.APPLICATION_YAML_VALUE})
+                    MediaType.APPLICATION_YAML_VALUE}
+    )
     @Override
     public PersonDTO create(@RequestBody PersonDTO person) {
         return service.create(person);
@@ -61,11 +65,13 @@ public class PersonController implements PersonControllerDocs {
             produces = {
                     MediaType.APPLICATION_JSON_VALUE,
                     MediaType.APPLICATION_XML_VALUE,
-                    MediaType.APPLICATION_YAML_VALUE})
+                    MediaType.APPLICATION_YAML_VALUE}
+    )
     @Override
     public PersonDTO update(@RequestBody PersonDTO person) {
         return service.update(person);
     }
+
 
     @DeleteMapping(value = "/{id}")
     @Override
